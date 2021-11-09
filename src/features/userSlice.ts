2fa15b921144bd1dc3721935bdd04cf57718c875
Shoiti Tsuchiya <murillo.mild@gmail.com>
2021-11-09 21:49:07 +0900

    make app config for auth comp & feed comp

--- src/features/userSlice.ts
+++ src/features/userSlice.ts
@@ -24,7 +24,7 @@ export const userSlice = createSlice({
 export const { login, logout } = userSlice.actions;
 
 
-export const selectCount = (state: RootState) => state.user.user;
+export const selectUser = (state: RootState) => state.user.user;
 
 
 
