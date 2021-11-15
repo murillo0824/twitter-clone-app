96141b6624ff37b2aa39ff1a34258d5d1121c5dc
Shoiti Tsuchiya <murillo.mild@gmail.com>
2021-11-15 20:40:11 +0900

    make logic to post tweet

--- src/components/Tweetinput.tsx
+++ src/components/Tweetinput.tsx
@@ -1,28 +1,84 @@
-import React from 'react';
-import styles from './Tweetinput.module.css';
-import { useSelector } from 'react-redux';
-import { selectUser } from '../features/userSlice';
-import {auth} from '../firebase';
+import React from "react";
+import styles from "./Tweetinput.module.css";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { auth, storage, db } from "../firebase";
 
-import { Avatar } from '@material-ui/core';
-
+import { Avatar, Button, IconButton } from "@material-ui/core";
+import { useState } from "react";
+import firebase from "firebase/app";
+import AddAPhotoIcon from "@material-ui/icons";
 
 const Tweetinput = () => {
   const user = useSelector(selectUser);
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState<File | null>(null);
+  const onChangeImageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files![0]) {
+      // files  に入っている！は　type script にnull の可能性がないことを示すことによってエラーを回避している
+      setTweetImage(e.target.files![0]);
+      e.target.value = "";
+    }
+  };
+  const sendTweet = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (tweetImage) {
+      const S =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      const N = 16;
+      const randamChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+        .map((n) => S[n % S.length])
+        .join("");
+
+      const fileName = randamChar + "_" + tweetImage.name;
+
+      const uploadTweatImg = storage.ref(`images/${fileName}`).put(tweetImage);
+      uploadTweatImg.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        () => {},
+        (err) => {
+          alert(err.message);
+        },
+        async () => {
+          await storage
+            .ref("images")
+            .child(fileName)
+            .getDownloadURL()
+            .then(async (url) => {
+              await db.collection("posts").add({
+                avatar: user.photoUrl,
+                image: url,
+                text: tweetMessage,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                username: user.displayName,
+              });
+            });
+        }
+      );
+    } else {
+      db.collection("posts").add({
+        avatar: user.photoUrl,
+        image: "",
+        text: tweetMessage,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        username: user.displayName,
+      });
+      setTweetImage(null);
+      setTweetMessage("");
+    }
+  };
 
   return (
     <div>
       <Avatar
         className={styles.tweet_avatar}
         src={user.photoUrl}
-        onClick={
-          async () =>{
-            await auth.signOut();
-          }
-        }
+        onClick={async () => {
+          await auth.signOut();
+        }}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Tweetinput
+export default Tweetinput;
