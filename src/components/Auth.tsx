dd91374e345d294da3e31a8c512a6013195331b9
Shoiti Tsuchiya <murillo.mild@gmail.com>
2021-11-15 15:56:54 +0900

    make Email login logic in Auth components

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,50 +1,59 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { auth, provider, storage } from "../firebase";
 
-
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import SendIcon from "@material-ui/icons/Send";
 import CameraIcon from "@material-ui/icons/Camera";
 import EmailIcon from "@material-ui/icons/Email";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
-import styles from './Auth.module.css'
-
+import styles from "./Auth.module.css";
 
 const theme = createTheme();
 
-const Auth:React.FC = ()=> {
+const Auth: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const signInEmail = async () => {
+    await auth.signInWithEmailAndPassword(email, password);
+  };
+
+  const signUpEmail = async () => {
+    await auth.createUserWithEmailAndPassword(email, password);
+  };
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     // eslint-disable-next-line no-console
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
-  const signInGoogle = async () =>{
+  const signInGoogle = async () => {
     await auth.signInWithPopup(provider).catch((err) => alert(err));
   };
 
-
   return (
     <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
         <Grid
           item
@@ -52,12 +61,15 @@ const Auth:React.FC = ()=> {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://images.unsplash.com/photo-1636482095080-4033a0cd51de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80)',
-            backgroundRepeat: 'no-repeat',
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1636482095080-4033a0cd51de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80)",
+            backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -65,18 +77,23 @@ const Auth:React.FC = ()=> {
             sx={{
               my: 8,
               mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
-              Sign in
+              {isLogin ? "Sign in" : "Register"}
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
               <TextField
                 margin="normal"
                 required
@@ -86,6 +103,11 @@ const Auth:React.FC = ()=> {
                 name="email"
                 autoComplete="email"
                 autoFocus
+                value={email}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setEmail(e.target.value);
+                }}
+                
               />
               <TextField
                 margin="normal"
@@ -96,16 +118,53 @@ const Auth:React.FC = ()=> {
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                value={password}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setPassword(e.target.value);
+                }}
               />
-              
+
               <Button
-                type="submit"
+                
                 fullWidth
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
+                startIcon={<EmailIcon />}
+                onClick={
+                  isLogin
+                    ? async () => {
+                      try{
+                        await signInEmail();
+                      } catch(err:any){
+                        alert(err.message)
+                      }
+                    }
+                  : async () =>{
+                    try {
+                      await signUpEmail();
+                    } catch(err:any){
+                      alert(err.message)
+                    }
+                  }
+                }
               >
-                Sign In
+                {isLogin ? "Sign in" : "Register"}
               </Button>
+
+              <Grid container>
+                <Grid item xs>
+                  <span className={styles.login_reset}>Forgot password?</span>
+                </Grid>
+                <Grid item xs>
+                  <span 
+                  onClick={()=> setIsLogin(!isLogin)}
+                  className={styles.login_toggleMode}
+                  >
+                  {isLogin ? "Create new account?" : "Back to login"}
+                  </span>
+                </Grid>
+
+              </Grid>
 
               <Button
                 fullWidth
@@ -115,15 +174,12 @@ const Auth:React.FC = ()=> {
               >
                 Sign In with Google
               </Button>
-              
-              
-              
             </Box>
           </Box>
         </Grid>
       </Grid>
     </ThemeProvider>
   );
-}
+};
 
 export default Auth;
